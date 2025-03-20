5617b374d9471df549c876c1f93640e0e9a216a5
Richart Escobedo Quispe <rescobedoq@unsa.edu.pe>
2025-03-20 12:49:16 -0500

    Termine el lab01 - ingreso de datos

---
+++ lab01/Program.java
@@ -0,0 +1,28 @@
+public class Program{
+
+	double x;
+	double y;
+	double result;
+
+	public Program(double a, double b){
+		this.x = a;
+		this.y = b;
+
+	}
+
+	public void  setX(double a) {
+		this.x = a;
+	}
+	
+	public void  setY(double b) {
+		this.y = b;
+	}
+
+	public double  getResult(){
+		return this.result;
+	}
+
+	public double  getAdd(){
+		return this.x+this.y;
+	}
+}
